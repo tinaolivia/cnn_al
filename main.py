a51f58ca24d400e9d6650eaf42e074e0d8332db6
tinaolivia <tina.os.oftedal@gmail.com>
2019-03-18 09:29:48 +0100

    small changes

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ csv.field_size_limit(sys.maxsize)
 parser = argparse.ArgumentParser(description='CNN text classificer')
 # learning
 parser.add_argument('-lr', type=float, default=0.001, help='initial learning rate [default: 0.001]')
-parser.add_argument('-epochs', type=int, default=10, help='number of epochs for train [default: 256]')
+parser.add_argument('-epochs', type=int, default=50, help='number of epochs for train [default: 50]')
 parser.add_argument('-batch-size', type=int, default=64, help='batch size for training [default: 64]')
 parser.add_argument('-log-interval',  type=int, default=1,   help='how many steps to wait before logging training status [default: 1]')
 parser.add_argument('-test-interval', type=int, default=100, help='how many steps to wait before testing [default: 100]')
